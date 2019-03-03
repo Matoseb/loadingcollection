d7e19e40c23d676975e9e6f21c104ed77a11c3ee
Sébastien Matos <matos.sebastien@gmail.com>
2019-03-03 20:11:00 +0100

    Movable cursors

---
+++ resources/custom_cursor.js
@@ -0,0 +1,9 @@
+addEventListener('load', _ => {
+    var s = document.body.firstElementChild,
+        f = e => s.style.cssText = `position:absolute;top:0;left:0;transform:translate(${e.clientX-5}px,${e.clientY-5}px)`;
+    addEventListener('wheel', f, { passive: true });
+    addEventListener('mousemove', f);
+    addEventListener('touchend', e => e.preventDefault());
+    document.addEventListener('mouseleave', _ => s.removeAttribute('style'));
+    document.body.style = 'cursor: none;';
+}, { once: true });
