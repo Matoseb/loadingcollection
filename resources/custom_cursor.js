df93df7b77a856119156e58cbaeeb071ec88dc81
Sébastien Matos <matos.sebastien@gmail.com>
2019-03-03 20:19:55 +0100

    Fix mouseleave

--- resources/custom_cursor.js
+++ resources/custom_cursor.js
@@ -4,6 +4,6 @@ addEventListener('load', _ => {
     addEventListener('wheel', f, { passive: true });
     addEventListener('mousemove', f);
     addEventListener('touchend', e => e.preventDefault());
-    document.addEventListener('mouseleave', _ => s.removeAttribute('style'));
+    document.body.addEventListener('mouseleave', _ => s.removeAttribute('style'));
     document.body.style = 'cursor: none;';
 }, { once: true });
